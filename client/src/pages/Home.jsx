e8d4bd94f0f291ab62abb63c51825ebc302715ce
ishuklaji <ishuklaji100@gmail.com>
2026-02-25 02:46:37 +0100

    first push

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Slider from "../components/Slider/Slider";
+
+const Home = () => {
+  return (
+    <>
+      {/*slider  */}
+      <Slider />
+    </>
+  );
+};
+
+export default Home;
