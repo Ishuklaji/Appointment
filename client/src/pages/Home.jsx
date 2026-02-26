497ae2517e464ca7e6ece504ab5a994586c186ea
ishuklaji <ishuklaji100@gmail.com>
2026-02-26 02:44:24 +0100

    work in progress

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Slider from "../components/Slider/Slider";
+import Facility from "../components/Static/Facility/Facility";
+import ShortIntro from "../components/Static/ShortIntro/ShortIntro";
 
 const Home = () => {
   return (
     <>
       {/*slider  */}
       <Slider />
+      {/* facility */}
+      <Facility />
+      {/* short hospital intro */}
+      <ShortIntro />
     </>
   );
 };
