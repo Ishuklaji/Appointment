429fb7d9f4f15968b2b6f0574f14130da0a72441
ishuklaji <ishuklaji100@gmail.com>
2026-04-05 00:38:38 +0200

    working

--- controllers/userController.js
+++ controllers/userController.js
@@ -179,7 +179,7 @@ export const updatePassword = async (req, res) => {
       });
     }
 
-    // hashing
+    // hashing the password
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(newPassword, salt);
