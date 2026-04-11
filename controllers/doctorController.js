6fa1b206701e637c04d7869904a807a268d49a8f
ishuklaji <ishuklaji100@gmail.com>
2026-04-11 03:47:54 +0200

    doctor controllers added

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -74,3 +74,136 @@ export const addDoctor = async (req, res) => {
     });
   }
 };
+
+// Get All doctors
+export const getAllDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({});
+    res.status(200).send({
+      success: true,
+      message: "All doctors fetched successfully",
+      totalCount: doctors.length,
+      doctors,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in fetching all doctors API",
+      error,
+    });
+  }
+};
+
+// Get single doctor details
+export const getDoctorDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(404).send({
+        success: false,
+        message: "Please provide doctor ID",
+      });
+    }
+
+    const doctor = await doctorModel.findById(id);
+    if (!doctor) {
+      return res.status(404).send({
+        success: false,
+        message: "Doctor not found",
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      message: "Doctor details fetched successfully",
+      doctor,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in fetching doctor details API",
+      error,
+    });
+  }
+};
+
+// Update doctor details
+export const updateDoctor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(404).send({
+        success: false,
+        message: "Please provide doctor ID",
+      });
+    }
+
+    const data = req.body;
+
+    const photoToBase64 = req.file
+      ? req.file.buffer.toString("base64")
+      : data.image;
+
+    const updatedDoctor = await doctorModel.findByIdAndUpdate(
+      id,
+      { ...req.body, image: photoToBase64 },
+      { new: true },
+    );
+
+    if (!updatedDoctor) {
+      return res.status(404).send({
+        success: false,
+        message: "Doctor not found",
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      message: "Doctor details updated successfully",
+      doctor: updatedDoctor,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in updating doctor details API",
+      error,
+    });
+  }
+};
+
+// Delete doctor
+export const deleteDoctor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(404).send({
+        success: false,
+        message: "Please provide doctor ID",
+      });
+    }
+
+    const deletedDoctor = await doctorModel.findByIdAndDelete(id);
+
+    if (!deletedDoctor) {
+      return res.status(404).send({
+        success: false,
+        message: "Doctor not found",
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      message: "Doctor deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in deleting doctor API",
+      error,
+    });
+  }
+};
--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,11 +1,35 @@
 import express from "express";
 import { isAdmin, userAuth } from "../middlewares/AuthMiddleware.js";
-import { addDoctor } from "../controllers/doctorController.js";
+import {
+  addDoctor,
+  deleteDoctor,
+  getAllDoctors,
+  getDoctorDetails,
+  updateDoctor,
+} from "../controllers/doctorController.js";
 import upload from "../middlewares/multer.js";
 
 const router = express.Router();
 
 // ADD DOCTOR || POST
 router.post("/add", userAuth, isAdmin, upload.single("image"), addDoctor);
+
+// GET ALL DOCTORS || GET
+router.get("/get-all", getAllDoctors);
+
+// GET SINGLE DOCTOR || GET
+router.get("/get-details/:id", getDoctorDetails);
+
+// UPDATE DOCTOR DETAILS || PATCH
+router.patch(
+  "/update/:id",
+  userAuth,
+  isAdmin,
+  upload.single("image"),
+  updateDoctor,
+);
+
+// DELETE DOCTOR || DELETE
+router.delete("/delete/:id", userAuth, isAdmin, deleteDoctor);
 
 export default router;
